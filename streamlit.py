20643aa2b51cfd632b0ce57f8e98678ae4fd40b7
taranveer-12 <veer18singh19@gmail.com>
2025-05-27 16:50:35 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,19 +1,15 @@
 import streamlit as st
-from get_stock_data import get_stock_summary
-from get_news import get_top_news
-from retriever import retrieve_insights
-from summarize import make_summary
+import streamlit as st
+import requests
 # from voice_output import speak
 
 st.title("Finance Assistant")
 
 if st.button("Get Market Brief"):
-
-    stock_info = get_stock_summary("TSMC")
-    news = get_top_news("TSMC")
-    filtered_news = retrieve_insights(news, "earnings")
-    brief = make_summary(stock_info, filtered_news)
+    res = requests.get("http://localhost:8000/get_brief")  
+    brief = res.json()["brief"]
     st.write(brief)
     # speak(brief)
 
+    
 
