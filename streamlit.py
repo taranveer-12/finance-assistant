6f6d0d63143d3196510051ee28a7a7f6264d9d54
taranveer-12 <veer18singh19@gmail.com>
2025-05-27 16:37:37 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,11 +1,15 @@
 import streamlit as st
+import streamlit as st
 import requests
-from voice_output import speak
+# from voice_output import speak
 
 st.title("Finance Assistant")
 
 if st.button("Get Market Brief"):
-    res = requests.get("http://localhost:8000/get_brief")
+    res = requests.get("http://localhost:8000/get_brief")  
     brief = res.json()["brief"]
     st.write(brief)
-    speak(brief)
+    # speak(brief)
+
+    
+
